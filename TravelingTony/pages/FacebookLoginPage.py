f4195d1cb18b6e158f3e1057d987e5b6e79792e4
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-12 23:25:27 -0800

    Import error issues

--- TravelingTony/pages/FacebookLoginPage.py
+++ TravelingTony/pages/FacebookLoginPage.py
@@ -1,27 +1,41 @@
-from selenium                                 import webdriver
-from selenium.webdriver.support.ui            import WebDriverWait
-from selenium.webdriver.support.select        import Select
-from selenium.webdriver.support               import expected_conditions as EC
-from selenium.webdriver.common.by             import By
-from abc                                      import abstractmethod
-from TravelingTony.Constants                  import LocatorMode
-from BasePage import BasePage
+from BasePage                import BasePage
+from ShareOnFacebookPage     import ShareOnFacebookPage
+from BasePage                import IncorrectPageException
+
 
 class FacebookLoginPage(BasePage):
 
-  def __init__(self, drive, username, password):
-    super(FacebookLoginPage, self).__init__(driver)
-    self.username = username
-    self.password = password
+    def __init__(self, driver, username, password):
+        super(FacebookLoginPage, self).__init__(driver)
+        self.username = username
+        self.password = password
 
-  def _verify_page(self):
-    try:
-      self.wait_for_element_visibility(10, "id", "wsite-com-product-option-Quantity")
-    except:
-      raise IncorrectPageException  #Used if we don't find the correct page
+    def _verify_page(self):
+        try:
+          self.wait_for_element_visibility(10, 
+                                           "id", 
+                                           "email"
+          )
+        except:   
+          raise IncorrectPageException
+    
+    def login(self):
+        self.fill_out_field("id", 
+                            "email", 
+                            self.username
+        )
+        self.fill_out_field("id", 
+                            "pass", 
+                            self.password
+        )
+        self.click(10, "name", "login")
+        return ShareOnFacebookPage(self.driver)
+        
+        
+    
+      
+    
 
-  #Locate the field using email
-  def login(self):
-    self.fill_out_field("id", "email", self.username)
-    self.fill_out_field("id", "pass", self.password)
-    self.click(10, "name", "login")
+
+
+
--- TravelingTony/pages/ProductPage.py
+++ TravelingTony/pages/ProductPage.py
@@ -1,28 +1,38 @@
-from selenium                                 import webdriver
-from selenium.webdriver.support.ui            import WebDriverWait
-from selenium.webdriver.support.select        import Select
-from selenium.webdriver.support               import expected_conditions as EC
-from selenium.webdriver.common.by             import By
-from abc                                      import abstractmethod
-from TravelingTony.Constants                  import LocatorMode
-from BasePage import BasePage
+from BasePage                import BasePage
+from FacebookLoginPage       import FacebookLoginPage
+from TravelingTony.Constants import TT_Constants
+from BasePage                import IncorrectPageException
+
 
 class ProductPage(BasePage):
 
-  def __init__(self, driver):
-    super(ProductPage, self).__init__(driver)
+    def __init__(self, driver):
+        super(ProductPage, self).__init__(driver)
+  
 
-  def _verify_page(self):
-    try:
-      self.wait_for_element_visibility(10, "id", "wsite-com-product-option-Quantity")
-    except:
-      raise IncorrectPageException  #Used if we don't find the correct page
+    def _verify_page(self):
+        try:
+          self.wait_for_element_visibility(10, 
+                                         "id", 
+                                         "wsite-com-product-option-Quantity"
+          )
+        except:   
+          raise IncorrectPageException
+    
+    def click_share_on_facebook_button(self):
+        mainWindowHandle = self.driver.window_handles
+        self.click(10, "xpath", "//a[@title='Share on Facebook']")
+        allWindowHandles = self.driver.window_handles
+        for handle in allWindowHandles:
+         if handle != mainWindowHandle[0]:
+          self.switch_to_window(handle)
+          break
+        return FacebookLoginPage(self.driver, 
+                                 TT_Constants['Facebook_Username'],
+                                 TT_Constants['Facebook_Password'] 
+        )
+        
+    
+      
+    
 
-  def click_on_facebook_share_button(self):
-    mainWindowHandle = self.driver.window_handles
-    self.click(10, "xpath", "//a[@titles='Share on Facebook']") #Click on FB share buton
-    allWindowHandles = self.driver.window_handles
-    for handle in allWindowHandles:
-    if handle != mainWindowHandle[0]:
-      self.switch_to_window(handle)
-      break
